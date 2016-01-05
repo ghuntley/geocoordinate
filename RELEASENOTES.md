487f00c77a3b7609443757903e0ae2c3ca5108bc
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-01-05 20:10:27 +1100

    initial commit

---
+++ RELEASENOTES.md
@@ -0,0 +1,2 @@
+### New in 0.1.0 (released 2016/01/05)
+* Initial release
---
+++ src/GeoCoordinatePortable/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("GeoCoordinate")]
+[assembly:
+    AssemblyDescription(
+        "GeoCoordinate is a Portable Class Library compatible implementation of System.Device.Location.GeoCoordinate. It is an exact 1:1 API compliant implementation and will be supported until MSFT sees it fit to embed the type. Which at that point this implementation will cease development/support and you will be able to simply remove this package and everything will still work."
+        )]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://ghuntley.com/; ghuntley@ghuntley.com")]
+[assembly: AssemblyProduct("GeoCoordinate")]
+[assembly: AssemblyCopyright("Copyright © Geoffrey Huntley 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
